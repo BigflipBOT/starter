427166917e3b9eb3db80402c588a17395cf45fca
BigflipBOT <flipypr@protonmail.com>
2025-07-17 20:42:17 +0200

    added rust-analyzer

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -10,7 +10,7 @@ lspconfig.servers = {
 }
 
 -- list of servers configured with default config.
-local default_servers = { "html", "cssls" }
+local default_servers = { "cssls", "html", "rust_analyzer" }
 
 -- lsps with default config
 for _, lsp in ipairs(default_servers) do
