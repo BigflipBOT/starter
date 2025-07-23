024c1503b4dae919ec5e2f4d93a63391f05767a0
BigflipBOT <flipypr@protonmail.com>
2025-07-23 12:54:51 +0200

    terminal configuration

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,17 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "catppuccin",
+    theme = "catppuccin",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    -- hl_override = {
+    -- 	Comment = { italic = true },
+    -- 	["@comment"] = { italic = true },
+    -- },
 }
 
 -- M.nvdash = { load_on_startup = true }
@@ -20,5 +20,17 @@ M.base46 = {
 --          lazyload = false
 --      }
 --}
+
+M.term = {
+    winopts = { number = false },
+    sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+    float = {
+        row = 0.15,
+        col = 0.25,
+        width = 0.5,
+        height = 0.6,
+        border = "double",
+    },
+}
 
 return M
